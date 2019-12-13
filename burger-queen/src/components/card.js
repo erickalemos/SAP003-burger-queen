6a340ee1f8b799c60f191ba278ba4763766b2fcc
erickalemos <ericka.florentino@gmail.com>
2019-12-13 12:22:58 -0300

    criação do firebase no projeto e react

---
+++ burger-queen/src/components/card.js
@@ -0,0 +1,9 @@
+// function Menuitems(props) {
+//     const Menuitems = `
+//     <form class="form">
+//       <Menuitems data-id="${props.dataId}" class="${props.class}" type="${props.type}" " placeholder="${props.placeholder}"/>    
+//     </form>
+//     `;
+//     return Menuitems;
+//   }
+//   export default Menuitems;
---
+++ burger-queen/src/pages/waiter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+// db.collection('menu').get()
+//   .then((snapshot) => {
+//     snapshot.forEach((doc) => {
+//       console.log(doc.data());
+//     });
+//   })
+//   .catch((err) => {
+//     console.log('Error getting documents', err);
+//   });
+
+const template = `
+<div class="card">
+<header class="container blue">
+  <h1>Pedido</h1> 
+</header>
+
+<div class="w3-container">
+  <p>"Lorem ipsum..."</p>
+</div>
+
+<footer class="container blue">
+
+</footer>
+`
