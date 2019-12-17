16785766de50a61ec8307547798e90b580efef52
erickalemos <ericka.florentino@gmail.com>
2019-12-17 19:58:11 -0300

    arquivos em novo caminho

--- burger-queen/src/pages/waiter.js
+++ burger-queen/src/pages/waiter.js
@@ -11,17 +11,4 @@ import './App.css';
 //     console.log('Error getting documents', err);
 //   });
 
-const template = `
-<div class="card">
-<header class="container blue">
-  <h1>Pedido</h1> 
-</header>
-
-<div class="w3-container">
-  <p>"Lorem ipsum..."</p>
-</div>
-
-<footer class="container blue">
-
-</footer>
 `
