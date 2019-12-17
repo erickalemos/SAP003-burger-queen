9c23bc2345153e7388ee85a5daa68ac336611d53
erickalemos <ericka.florentino@gmail.com>
2019-12-17 20:05:47 -0300

    arquivos em novo caminho

--- src/components/card.js
+++
@@ -1,9 +0,0 @@
-// function Menuitems(props) {
-//     const Menuitems = `
-//     <form class="form">
-//       <Menuitems data-id="${props.dataId}" class="${props.class}" type="${props.type}" " placeholder="${props.placeholder}"/>    
-//     </form>
-//     `;
-//     return Menuitems;
-//   }
-//   export default Menuitems;
--- src/components/order.js
+++
@@ -1,9 +0,0 @@
-// function Order(props) {
-//     const order = `
-//     <form class="form">
-//       <order data-id="${props.dataId}" class="${props.class}" type="${props.type}" " placeholder="${props.placeholder}"/>    
-//     </form>
-//     `;
-//     return order;
-//   }
-//   export default Order;
