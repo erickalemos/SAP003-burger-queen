c3b73b4b3aea92fa74d4a2a0faf8ae42f5a32336
erickalemos <ericka.florentino@gmail.com>
2020-01-30 00:56:19 -0300

    atualização de diretório

---
+++ build/precache-manifest.acc195cbfa033cc8348b9aa675c8a4a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "173f13717dc540635eeb1f2cb4bf8672",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10fa493d5fb08c00cdb7",
+    "url": "/static/css/main.a3bfaaae.chunk.css"
+  },
+  {
+    "revision": "c423178b7ff1d822b575",
+    "url": "/static/js/2.972c3a12.chunk.js"
+  },
+  {
+    "revision": "f73e810844fba009bd106bf78cf93a52",
+    "url": "/static/js/2.972c3a12.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10fa493d5fb08c00cdb7",
+    "url": "/static/js/main.394d3940.chunk.js"
+  },
+  {
+    "revision": "4c8b79c4ddb3b3e5cead",
+    "url": "/static/js/runtime-main.f7663895.js"
+  }
+]);
